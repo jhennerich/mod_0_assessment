7bfaff7afa55a93057cd0c5ed4ab09f8cc4bc0ff
John Hennerich <john.hennerich@gmail.com>
2021-11-12 11:15:33 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,18 @@
+#create anmial variable  (looks like a typo of animal but I am following the instructions)
+anmial = "dog"
+is_cuddly = true
+
+anmial = "cat"
+num_8 = 3
+num_3 = 9
+
+quotient = num_3 / num_8
+#puts quotient
+
+favorite_films = ["Star Wars","Sound of Music","LA Story"]
+#puts favorite_films
+
+favorite_films.shift()
+#puts favorite_films
+
+puts "There are #{anmial.length()} chars in #{anmial}"
