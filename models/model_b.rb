8c7520f6cb354c403eaab615dfc7ce96a4a02256
John Hennerich <john.hennerich@gmail.com>
2021-11-12 11:40:25 -0700

    Add house model

---
+++ models/model_b.rb
@@ -0,0 +1,14 @@
+# create a model house using hash
+
+house = {
+  "street" => "Newton",
+  "owner" => "John",
+  "zipcode" => 80204,
+  "taxrate" => 10.0,
+  "forrent" => false,
+  "forsale" => false,
+  "occupents" => ["John","Deb"],
+  "rooms" => ["Master Bedroom","Office","Bathroom","Guest Bedroom"]
+}
+
+#puts house
