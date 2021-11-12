8463ebf06aa6045f7cd2b3369e7f2bbf4188c274
John Hennerich <john.hennerich@gmail.com>
2021-11-12 11:29:53 -0700

    Add laptop model

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+#Model a Laptop using a Hash
+
+laptop ={
+  "model" => "MacBook Pro",
+  "make" => "Apple",
+  "cost" => 1200.00,
+  "ports" => 2,
+  "new" => true,
+  "preowned" => false,
+  "previous_owner" => ["John","Paul","George","Ringo"],
+  "OS" => ["Catalina","Big Sur","Monterey"]
+}
